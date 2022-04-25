e1d5c54ab3bff3c0efcd09da9cb732055e441d74
Cartoony <104101690+ItsCartoony@users.noreply.github.com>
2022-04-24 23:04:29 -0400

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,102 @@
+const logotext = "CARTOONY";
+const meta = {
+    title: "Cartoony",
+    description: "I’m Cartoony, a data scientist, and Full stack devloper, currently working for ExploitX",
+};
+
+const introdata = {
+    title: "I’m Cartoony",
+    animated: {
+        first: "I love coding",
+        second: "I code cool sites",
+        third: "I develop console cheats",
+    },
+    description: "I am Cartoony, an anonymous Full-Stack developer.",
+    your_img_url: "https://avatars.githubusercontent.com/u/104101690?v=4",
+};
+
+const dataabout = {
+    title: "a bit about myself",
+    aboutme: "I am Cartoony, an anonymous Full-Stack developer. I am a minor, who loves to play video games, and code. My favourite food is Chicken Curry.",
+};
+const worktimeline = [{
+        jobtitle: "Team Member",
+        where: "Worldwide",
+        date: "2022",
+    },
+    {
+        jobtitle: "Team Member",
+        where: "Worldwide",
+        date: "2021",
+    },
+    {
+        jobtitle: "Team Member",
+        where: "Worldwide",
+        date: "2020",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 99.9,
+    },
+    {
+        name: "Djano",
+        value: 25,
+    },
+    {
+        name: "Javascript",
+        value: 9999,
+    },
+    {
+        name: "React",
+        value: 100,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "I don't do services ..",
+        description: "I don't do services ..",
+    }
+];
+
+const dataportfolio = [{
+        img: "https://github.com/ItsCartoony/readme/blob/main/hey.jpg?raw=true",
+        desctiption: "A 'game blocker' to trick you parents into thinking all your games were deleted.",
+        link: "https://itscartoony.github.io/FakeGameBlocker/",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "cartoonyish73@gmail.com",
+    YOUR_FONE: "(929) 920-6148",
+    description: "Contact me if you have question about my software, the for to the side does not work at the moment. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/ItsCartoony",
+    facebook: "#",
+    linkedin: "#",
+    twitter: "#",
+    youtube: "#",
+    twitch: "#",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
